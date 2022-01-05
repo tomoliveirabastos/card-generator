48fd0ad41be1e1b4ed746c0498d54e926d879365
Tom Oliveira <washington-kd@hotmail.com>
2022-01-05 01:02:45 -0300

    coloquei um redirect

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -10,7 +10,15 @@ class MainController extends Controller
     /**
      * @Get("/", middleware="web")
      */
-    public function index(Request $request)
+    public function index()
+    {
+        return redirect('/gerador-de-licencas');
+    }
+
+    /**
+     * @Get("gerador-de-licencas", as="gerador_de_licencas", middleware="web")
+     */
+    public function geradorDeLicencas()
     {
         return view('main');
     }
